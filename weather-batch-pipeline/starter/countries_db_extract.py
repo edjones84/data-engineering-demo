3eb4eb76786786848807b9948cd495c76e9bf7c8
bennyelekwa <bennettelekwa@gmail.com>
2025-07-02 13:27:25 +0100

    clear starter files

--- weather-batch-pipeline/starter/countries_db_extract.py
+++ weather-batch-pipeline/starter/countries_db_extract.py
@@ -7,8 +7,7 @@ def extract_countries_json_from_worldbank_api(url: str):
     '''
     Extracts Countries JSON data from worldbank API
     '''
-    response = requests.get(url)
-    return response.json()[1]
+    pass
 
 
 
@@ -17,9 +16,7 @@ def connect(database_name: str):
     ''' 
     Connects to SQLlite and creates specified database object
     '''
-    conn = sqlite3.connect(database_name)
-    cur = conn.cursor()
-    return cur, conn
+    pass
 
 
 
@@ -30,22 +27,7 @@ def load_data_to_sqllite_db(cursor, connect, countries_json):
     Inserts data into countries table from countries json response of worldbank API
     '''
 
-    try:
-        # Create table
-        cursor.executescript('''
-        DROP TABLE IF EXISTS countries;
-
-        CREATE TABLE countries (id varchar(5), name varchar(60), capital_city varchar(60))
-        ''')
-
-        # insert into table
-        for country in countries_json:
-            cursor.execute('INSERT INTO countries values (?,?,?)',
-                            [country['id'], country['name'], country['capitalCity']])
-            connect.commit()
-
-    except Exception as e:
-        print('Data Load Error : ' + str(e))
+    pass
 
 
 
@@ -54,19 +36,12 @@ def export_to_csv(connect, path: str):
     '''
     Read all Data from countries table and export to CSV
     '''
-
-    countries_data = pd.read_sql_query(''' select *
-                                            from countries;
-                                         ''', connect)
-    return countries_data.to_csv(path, index=False)
+    pass
 
 
 
 if __name__ == '__main__':
     try:
-        countries_res = extract_countries_json_from_worldbank_api('http://api.worldbank.org/countries?format=json&per_page=100')
-        cur, conn = connect('pythonEtlDemo') 
-        load_data_to_sqllite_db(cur,conn, countries_res)
-        export_to_csv(conn, 'countriesData.csv')
+        ''' Main function to run the ETL process '''
     except Exception as e:
-        print('Pipeline error: ' + str(e))
+        ''' Handle any exceptions that occur during the ETL process '''
--- weather-batch-pipeline/starter/dagster_countries_etl.py
+++ weather-batch-pipeline/starter/dagster_countries_etl.py
@@ -6,41 +6,18 @@ START_TIME = datetime(2025, 6, 12, 6, 58, tzinfo=timezone.utc)
 
 @op
 def extract_op(context):
-    url = 'http://api.worldbank.org/countries?format=json&per_page=100'
-    countries_res = countries_db_extract.extract_countries_json_from_worldbank_api(url)
-    context.log.info(f"Extracted {len(countries_res)} countries")
-    return countries_res
+    pass
 
 @op
 def load_op(context, countries_res):
-    cur, conn = countries_db_extract.connect('pythonEtlDemo')
-    countries_db_extract.load_data_to_sqllite_db(cur, conn, countries_res)
-    context.log.info("Loaded data to SQLite DB")
-    return 'pythonEtlDemo'
+    pass
 
 @op
 def export_op(context, db_path):
-    conn = countries_db_extract.connect(db_path)[1]
-    countries_db_extract.export_to_csv(conn, 'countriesData.csv')
-    context.log.info("Exported data to countriesData.csv")
+    pass
 
 @job
 def countries_etl_job():
-    countries = extract_op()
-    db_path = load_op(countries)
-    export_op(db_path)
+    pass
 
 
-
-basic_schedule = ScheduleDefinition(
-    job=countries_etl_job, 
-    cron_schedule="*/5 * * * *",
-    execution_timezone="UTC",
-    default_status=DefaultScheduleStatus.RUNNING,
-    description="Runs the countries ETL job every 5 minutes"
-)
-
-defs = Definitions(
-    jobs=[countries_etl_job],
-    schedules=[basic_schedule],
-)
